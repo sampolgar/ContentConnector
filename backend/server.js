51968d5dcf96516e445b33c6b7d5df667026f286
Sam Polgar <sampolgar@gmail.com>
2022-09-23 14:35:14 +1000

    ignore node modules

--- backend/server.js
+++ backend/server.js
@@ -11,18 +11,3 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use(errorHandler) //this will override default error handler
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-
-
-// //Routes
-// app.get('/', (req, res) => {
-//     res.send('We are on home');
-// });
-
-// app.get('/posts', (req, res) => {
-//     res.send('We are on posts');
-// });
-
-// //start listening
-
-// app.listen(3000);
