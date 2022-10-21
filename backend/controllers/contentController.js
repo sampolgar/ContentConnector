5c6419c8ef6b158e17e0664c39281ffc5ebebbf7
Sam Polgar <sampolgar@gmail.com>
2022-10-21 16:45:57 +1100

    updated DB queries again

--- backend/controllers/contentController.js
+++ backend/controllers/contentController.js
@@ -2,35 +2,62 @@ const asyncHandler = require("express-async-handler");
 const { restart } = require("nodemon");
 const Content = require("../model/contentModel");
 const ContentResponse = require("../model/contentResponseModel");
+const mongoose = require("mongoose");
 
 // @desc Get Content
 // @route GET /content
 const getContent = asyncHandler(async (req, res) => {
+  console.log("req.url ", JSON.stringify(req.originalUrl));
+
   const dbQuery = makeQuery(req.query);
-  const content = await Content.find(dbQuery)
-    .skip(req.query.skip)
-    .limit(req.query.limit);
-  res.status(200).json(content);
+
+  const totalQueryCount = await totalQueryCounter(dbQuery);
+
+  const currentQueryCount = await currentQueryCounter(dbQuery);
+
+  const content = await mongoFinder(dbQuery);
+
+  const offset = res.status(200).json(content);
 });
 
 const makeQuery = (query) => {
   const { contentType = "", parentId = "", search = "", ...unusedVars } = query;
-  //if search is empty, create the query based on the contentType and parentId
+
   let dbQuery = {};
   dbQuery.$and = [];
+  dbQuery.$or = [];
 
   if (search) {
     dbQuery.$text = { $search: `*${search}*` };
   }
   if (parentId) {
+    dbQuery.$or.push({ parentId: parentId });
     dbQuery.$and.push({ parentId: parentId.slice(-3) });
   }
   if (contentType) {
     dbQuery.$and.push({ contentType: contentType });
   }
-  //add folders in the DB query
+  console.log("dbQuery", dbQuery);
   return dbQuery;
 };
+
+const mongoFinder = async (dbQuery) => {
+  const content = await Content.find(dbQuery)
+    .skip(dbQuery.skip)
+    .limit(dbQuery.limit);
+  return content;
+};
+
+const currentQueryCounter = async (dbQuery) => {
+  
+}
+
+const totalQueryCounter = async (dbQuery) => {
+  const totalDocumentCount = await Content.countDocuments(dbQuery);
+  return totalDocumentCount;
+};
+
+const makeResponse = (contentResponse) => {};
 
 //@desc Set Content -- for testing only, use to post content to the db
 //@route POST /content
